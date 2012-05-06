b82200715629b6c6aca0eb61a123ac5d5e49a8cc
brian andrich <brianandrich@gmail.com>
2012-05-06 16:29:14 -0400

    added more randoms

--- js/script.js
+++ js/script.js
@@ -261,7 +261,7 @@ function anyScroll() {
 	$('.new-gallery').show();
 }
 
-var randomGallery = ["optimisto.tumblr.com", "bestgifarchive.tumblr.com", "influent.tumblr.com", "fuckyeahgifs.tumblr.com", "fancygifs.tumblr.com", "4gifs.tumblr.com", "gifpeanutbutter.tumblr.com"];
+var randomGallery = ["optimisto.tumblr.com", "bestgifarchive.tumblr.com", "influent.tumblr.com", "fuckyeahgifs.tumblr.com", "fancygifs.tumblr.com", "4gifs.tumblr.com", "gifpeanutbutter.tumblr.com", "wowfunniestposts.tumblr.com", "onlylolgifs.tumblr.com"];
 function getRandomGallery() {
    return randomGallery[Math.floor(Math.random() * randomGallery.length)];
 }
